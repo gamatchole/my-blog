4827a6fef5fd6d61177521a6b5624f627d97c171
Laurent CHAVANIS <laurent.chavanis@sap.com>
2019-07-23 13:44:33 +0200

    LCS First commit

---
+++ src/build/precache-manifest.f96f818dd67df4dcce83eb5b57d00486.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25bc62f6e49c4a580530ccf663d448a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64372001d8f3ba5595ee",
+    "url": "/static/css/main.1016fe27.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "64372001d8f3ba5595ee",
+    "url": "/static/js/main.eeab5f8b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,139 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {
+  MongoClient
+} from 'mongodb';
+import path from 'path';
+
+// const articlesInfo = {
+//   'learn-react' : {
+//     upvotes: 0,
+//     comments: []
+//   },
+//   'learn-node' : {
+//     upvotes: 0,
+//     comments: []
+//   },
+//   'my-thoughts-on-resumes' : {
+//     upvotes: 0,
+//     comments: []
+//   }
+// }
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json());
+
+const withDB = async (operations) => {
+  try {
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+      useNewUrlParser: true
+    });
+    const db = client.db('my-blog');
+
+    await operations(db);
+
+    client.close();
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error Connecting to DB!",
+      error
+    });
+  }
+};
+
+app.get('/api/articles/:name', async (req, res) => {
+  const articleName = req.params.name;
+
+  withDB(async (db) => {
+    const articleInfo = await db.collection('articles').findOne({
+      name: articleName
+    });
+    res.status(200).json(articleInfo);
+  }, res);
+
+});
+
+// TEST
+app.get('/hello', (req, res) => res.send('Hello!'));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name} !`));
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name} !`));
+// ENDTEST
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+
+  const articleName = req.params.name;
+
+  withDB(async (db) => {
+
+    // articlesInfo[articleName].upvotes += 1;
+    // res.status(200).send(`Article ${articleName} has ${articlesInfo[articleName].upvotes} upvotes!`);
+
+    const articleInfo = await db.collection('articles').findOne({
+      name: articleName
+    });
+
+    await db.collection('articles').updateOne({
+      name: articleName
+    }, {
+      "$set": {
+        upvotes: articleInfo.upvotes + 1
+      }
+    });
+
+    const updatedArticleInfo = await db.collection('articles').findOne({
+      name: articleName
+    });
+
+    res.status(200).json(updatedArticleInfo);
+
+  }, res);
+
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const articleName = req.params.name;
+  const {
+    username,
+    text
+  } = req.body;
+
+  withDB(async (db) => {
+
+    const articleInfo = await db.collection('articles').findOne({
+      name: articleName
+    });
+
+    await db.collection('articles').updateOne({
+      name: articleName
+    }, {
+      "$set": {
+        comments: articleInfo.comments.concat({
+          username,
+          text
+        })
+      }
+    });
+
+    const updatedArticleInfo = await db.collection('articles').findOne({
+      name: articleName
+    });
+
+    res.status(200).json(updatedArticleInfo);
+
+  }, res);
+  // articlesInfo[articleName].comments.push({ username, text });
+  // res.status(200).send(`Article ${articleName} has ${articlesInfo[articleName].comments.length} comments! With info = ${articlesInfo[articleName]}`);
+  // res.status(200).send(articlesInfo[articleName]);
+
+
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+app.listen(8000, console.log('Listening on port 8000!'));
